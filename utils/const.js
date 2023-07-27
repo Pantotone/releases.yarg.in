122d9f0824c2303179b39abfaa145f2c42119f9d
Panto! <guustavopantoja@gmail.com>
2023-07-27 20:19:18 -0300

    feat: launcher update.json updater

--- utils/const.js
+++ utils/const.js
@@ -1,5 +1,8 @@
 import path from "path";
 
+export const LAUNCHER_UPDATE_ORIGIN_URL = "https://gist.githubusercontent.com/EliteAsian123/7f0525ad6c1a8496b52ede4e41690fb7/raw/update.json";
+
 export const STATIC_PATH = "api/";
 export const STABLEYARG_RELEASES_PATH = path.join(STATIC_PATH, "StableYARG", "releases.json");
 export const NIGHTLYYARG_RELEASES_PATH = path.join(STATIC_PATH, "NightlyYARG", "releases.json");
+export const LAUNCHER_UPDATE_PATH = path.join(STATIC_PATH, "launcher", "update.json");
