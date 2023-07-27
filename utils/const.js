98b8691ca241f45bc70fde7196a9b440ee461c1e
Panto! <guustavopantoja@gmail.com>
2023-07-27 20:06:42 -0300

    feat: nightly yarg releases.json updater

--- utils/const.js
+++ utils/const.js
@@ -2,3 +2,4 @@ import path from "path";
 
 export const STATIC_PATH = "api/";
 export const STABLEYARG_RELEASES_PATH = path.join(STATIC_PATH, "StableYARG", "releases.json");
+export const NIGHTLYYARG_RELEASES_PATH = path.join(STATIC_PATH, "NightlyYARG", "releases.json");
