4c7f40e21cbd05b619227f51e42b4d22f7d85205
Panto! <guustavopantoja@gmail.com>
2023-08-15 04:53:10 -0300

    feat: api/NewEngine/releases.json updater

---
+++ methods/UpdateNewEngineYARG.js
@@ -0,0 +1,8 @@
+import fs from "fs/promises";
+import { GetSortedReleases } from "../utils/GetReleases.js";
+import { NEWENGINEYARG_RELEASES_PATH } from "../utils/const.js";
+
+const data = await GetSortedReleases("YARC-Official", "YARG-NewEngine");
+const releases = data.repository.releases.nodes;
+
+await fs.writeFile(NEWENGINEYARG_RELEASES_PATH, JSON.stringify(releases, null, 2));
