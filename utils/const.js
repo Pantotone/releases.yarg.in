18da2d8fa3e50dba0fed507ce13918bd85f43a87
Pantotone <guustavopantoja@gmail.com>
2024-07-14 02:07:58 -0300

    feat: new nightly releases

--- utils/const.js
+++ utils/const.js
@@ -1,10 +1,39 @@
 import path from "path";
 
-export const LAUNCHER_UPDATE_ORIGIN_URL = "https://gist.githubusercontent.com/EliteAsian123/7f0525ad6c1a8496b52ede4e41690fb7/raw/update.json";
+export const LAUNCHER_UPDATE_ORIGIN_URL =
+  "https://gist.githubusercontent.com/EliteAsian123/7f0525ad6c1a8496b52ede4e41690fb7/raw/update.json";
 
 export const STATIC_PATH = "publish";
-export const STABLEYARG_RELEASES_PATH = path.join(STATIC_PATH, "StableYARG", "releases.json");
-export const NIGHTLYYARG_RELEASES_PATH = path.join(STATIC_PATH, "NightlyYARG", "releases.json");
-export const NIGHTLYYARG_CHANGELOG_FOLDER = path.join(STATIC_PATH, "changelogs", "NightlyYARG");
-export const NEWENGINEYARG_RELEASES_PATH = path.join(STATIC_PATH, "NewEngine", "releases.json");
-export const LAUNCHER_UPDATE_PATH = path.join(STATIC_PATH, "launcher", "update.json");
+
+export const LAUNCHER_UPDATE_PATH = path.join(
+  STATIC_PATH,
+  "launcher",
+  "update.json"
+);
+export const STABLEYARG_RELEASES_PATH = path.join(
+  STATIC_PATH,
+  "StableYARG",
+  "releases.json"
+);
+export const NIGHTLYYARG_RELEASES_PATH = path.join(
+  STATIC_PATH,
+  "NightlyYARG",
+  "releases.json"
+);
+export const NEWENGINEYARG_RELEASES_PATH = path.join(
+  STATIC_PATH,
+  "NewEngine",
+  "releases.json"
+);
+
+export const NIGHTLYYARG_CHANGELOG_FOLDER = path.join(
+  STATIC_PATH,
+  "changelogs",
+  "NightlyYARG"
+);
+
+export const APPLICATIONS_PROFILES_FOLDER = path.join(
+  STATIC_PATH,
+  "profiles",
+  "application"
+);
